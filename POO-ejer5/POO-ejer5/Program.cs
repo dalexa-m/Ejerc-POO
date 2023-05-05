41dd01da96e6705b6a24e797e3f8fab1148da8d8
dalexa castillo <dalexa castillo@DESKTOP-7I9QU5C>
2023-05-04 23:11:50 -0600

    ejer6

---
+++ POO-ejer5/POO-ejer5/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ejer5
+{
+    class Foto
+    {
+        int id;
+        double tamamg;
+        DateTime fechacreacion;
+        string comentarios;
+
+        public Foto(int id, double tamamg, DateTime fechacreacion, string comentarios)
+        {
+            this.Id = id;
+            this.Tamamg = tamamg;
+            this.Fechacreacion = fechacreacion;
+            this.Comentarios = comentarios;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public double Tamamg { get => tamamg; set => tamamg = value; }
+        public DateTime Fechacreacion { get => fechacreacion; set => fechacreacion = value; }
+        public string Comentarios { get => comentarios; set => comentarios = value; }
+
+        public override string ToString()
+        {
+            return "Id: " + id + "\n" + "Tamaño MG: " + tamamg + "\n" + " fecha creacion: " + fechacreacion + "\n" + " Comentarios: " + comentarios;
+        }
+
+    }
+
+    class Album
+    {
+        int id;
+        string nombre;
+        DateTime fechacreacion;
+        List<Foto> Fotos;
+
+        public Album(int id, string nombre, DateTime fechacreacion)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.fechacreacion = fechacreacion;
+            this.Fotos = new List<Foto>();
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public DateTime Fechacreacion { get => fechacreacion; set => fechacreacion = value; }
+        internal List<Foto> Fotos1 { get => Fotos; set => Fotos = value; }
+
+        public void AñadirFoto(Foto f)
+        {
+            Fotos1.Add(f);
+        }
+
+        public void MostrarFoto()
+        {
+            foreach (var x in Fotos)
+            {
+                Console.WriteLine(x.ToString());
+            }
+        }
+        public void VaciarAlbum()
+        {
+            Fotos.Clear();
+        }
+
+        public void EliminarFoto(Foto f)
+        {
+            Fotos.Remove(f);
+        }
+
+        public double TamanoAlbum()
+        {
+            double tamanoTotal = 0;
+            foreach (var foto in Fotos)
+            {
+                tamanoTotal += foto.Tamamg;
+            }
+            return tamanoTotal / 1024; // Convertir a GB
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Foto fot1 = new Foto(1, 2.5, new DateTime(2022, 4, 15), "Una foto de la playa");
+            Foto fot2 = new Foto(2, 1.8, new DateTime(2022, 4, 16), "Una foto del campo");
+            Album a1 = new Album(1, "Vacaciones", new DateTime(2023, 5, 1));
+
+            Console.WriteLine("Añadir fotos:");
+            a1.AñadirFoto(fot1);
+            a1.AñadirFoto(fot2);
+            a1.MostrarFoto();
+            Console.WriteLine("-----");
+            Console.WriteLine("Eliminar foto: ");
+            a1.EliminarFoto(fot1);
+            a1.MostrarFoto();
+            Console.WriteLine("-----");
+            Console.WriteLine("Eliminar todas las fotos:");
+            a1.VaciarAlbum();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ POO-ejer6/POO-ejer6/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ejer6
+{
+   
+    public class Forma
+    {
+        
+        protected Ubicacion c;
+
+        
+        public Forma(double x, double y)
+        {
+            c = new Ubicacion(x, y);
+        }
+
+        // Métodos públicos
+        public override string ToString()
+        {
+            return $"Posición: ({c.x}, {c.y})";
+        }
+
+        public virtual double Area()
+        {
+            return 0;
+        }
+
+        public virtual double Perimetro()
+        {
+            return 0;
+        }
+    }
+
+    
+    public class Rectangulo : Forma
+    {
+        
+        private double lado1;
+        private double lado2;
+
+        
+        public Rectangulo(double x, double y, double lado1, double lado2) : base(x, y)
+        {
+            this.lado1 = lado1;
+            this.lado2 = lado2;
+        }
+
+        
+        public override string ToString()
+        {
+            return $"Rectangulo [{base.ToString()}] Lado1: {lado1}, Lado2: {lado2}";
+        }
+
+        public override double Area()
+        {
+            return lado1 * lado2;
+        }
+
+        public override double Perimetro()
+        {
+            return 2 * (lado1 + lado2);
+        }
+    }
+
+    
+    public class Circulo : Forma
+    {
+        
+        private double radio;
+
+        
+        public Circulo(double x, double y, double radio) : base(x, y)
+        {
+            this.radio = radio;
+        }
+
+        
+        public override string ToString()
+        {
+            return $"Circulo [{base.ToString()}] Radio: {radio}";
+        }
+
+        public override double Area()
+        {
+            return Math.PI * radio * radio;
+        }
+
+        public override double Perimetro()
+        {
+            return 2 * Math.PI * radio;
+        }
+    }
+
+    
+    public class Ubicacion
+    {
+        // Atributos públicos
+        public double x;
+        public double y;
+
+        // Constructor
+        public Ubicacion(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+   
+    public class Program
+    {
+        public static void Main()
+        {
+            
+            Rectangulo rect = new Rectangulo(3, 5, 2, 8);
+            Console.WriteLine(rect);
+            Console.WriteLine($"Área: {rect.Area()}");
+            Console.WriteLine($"Perímetro: {rect.Perimetro()}");
+
+            // Crear un objeto Circulo y mostrar su área y perímetro
+            Circulo circ = new Circulo(6, 8, 9);
+            Console.WriteLine(circ);
+            Console.WriteLine($"Área: {circ.Area()}");
+            Console.WriteLine($"Perímetro: {circ.Perimetro()}");
+        }
+    }
+}
