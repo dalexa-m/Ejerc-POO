768fbd281b45429e9d71de6021248ec578d73b1e
dalexa castillo <dalexa castillo@DESKTOP-7I9QU5C>
2023-05-04 22:36:50 -0600

    ejer3

--- POO.ejer2/POO.ejer2/Program.cs
+++ POO.ejer2/POO.ejer2/Program.cs
@@ -6,80 +6,136 @@ using System.Threading.Tasks;
 
 namespace POO.ejer2
 {
-    class Computadora
+    class Vehiculo
     {
 
-        int capadisco;
-        int espadisponible;
-        bool encendido;
 
+        int id;
+        string marca;
+        string modelo;
+        int km;
+        decimal precio;
+        int annio;
 
-
-        public Computadora(int capadisco, int espadisponible, bool encendido)
+        //contructor con sus propiedades
+        //
+        public Vehiculo() { }
+        public Vehiculo(int id, string marca, string modelo, int km, decimal precio, int annio)
         {
-            this.Capadisco = capadisco;
-            this.Espadisponible = espadisponible;
-            this.Encendido = encendido;
+            this.Id = id;
+            this.Marca = marca;
+            this.Modelo = modelo;
+            this.Km = km;
+            this.Precio = precio;
+            this.Annio = annio;
         }
 
-        public int Capadisco { get => capadisco; set => capadisco = value; }
-        public int Espadisponible { get => espadisponible; set => espadisponible = value; }
-        public bool Encendido { get => encendido; set => encendido = value; }
-        public void Añadirdatos(int datos)
-        {
-            if (encendido)
-            {
-                if (datos <= espadisponible)
-                {
-                    espadisponible -= datos;
-                    Console.WriteLine("Se añadio " + datos + " GB de datos al disco duro.\nEspacio disponible " + espadisponible + " GB");
-                }
-                else
-                {
-                    espadisponible = 0;
-                    Console.WriteLine("No hay espacio disponible");
-                }
-            }
-            else
-            {
-                Console.WriteLine("La computadora esta apagada");
-            }
-        }
+        public int Id { get => id; set => id = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public int Km { get => km; set => km = value; }
+        public decimal Precio { get => precio; set => precio = value; }
+        public int Annio { get => annio; set => annio = value; }
 
-        public void Eliminardatos(int datos)
+        public override string ToString()
         {
-            if (encendido)
-            {
-                if (datos <= (capadisco - espadisponible))
-                {
-                    espadisponible += datos;
-
-                    Console.WriteLine("Se elimino " + datos + " GB de datos del disco duro");
-                }
-                else
-                {
-                    espadisponible = capadisco;
-                    Console.WriteLine("No hay datos suficiente almacenados en el dico duro para eliminar. \n Espacio disponible " + espadisponible);
-                }
-            }
-            else
-            {
-                Console.WriteLine("La computadora esta apagada");
-            }
-        }
-
-        public void Apagar()
-        {
-            encendido = false;
-            Console.WriteLine("La computadora esta apagada");
-        }
-        public void Encendida()
-        {
-            encendido = true;
-            Console.WriteLine("La computadora esta encendida");
+            return "Modelo: " + modelo + "\n " + "Marca: " + marca + "\n" + "Kilometraje: " + km + "\n" + " Precio: " + precio + "Año: " + annio;
         }
     }
 
+    class CasaComercial
+    {
+        //campo de la clase casa comercial
+        string nombre;
+        string correo;
+        int totalv;
+
+        //esto  es una composicion ya que la clase casa comercial posee una lista de objetos de tipo vehiculos
+        List<Vehiculo> vehiculos;
+
+
+
+        //contructor 
+        public CasaComercial(string nombre, string correo, int totalv, List<Vehiculo> vehiculos)
+        {
+            this.Nombre = nombre;
+            this.Correo = correo;
+            this.Totalv = totalv;
+            this.vehiculos = vehiculos;
+        }
+
+        //propiedades
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Correo { get => correo; set => correo = value; }
+        public int Totalv { get => totalv; set => totalv = value; }
+        internal List<Vehiculo> Vehiculos { get => vehiculos; set => vehiculos = value; }
+
+
+        //añadimos cada uno de los metodos 
+        public void añadirVehiculo(Vehiculo V)
+        {
+            try
+            {
+                //parametro el objeto 
+                this.Vehiculos.Add(V); //añadimos un nuevo objeto a la lista vehiculo
+                this.totalv += 1; //actualizando el total de vehiculos
+                Console.WriteLine("se ha añadido un nuevo vehiculo");
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No se puede añadir debido a : \n" + ex.Message);
+                Console.ReadLine();
+            }
+
+
+        }
+
+
+        public void Mostrarvehiculo()
+        {
+            foreach (var item in this.vehiculos)
+            {
+                Console.WriteLine("---------");
+                item.ToString();
+            }
+        }
+
+        public void Vaciarvehiculos()
+        {
+
+
+
+            Console.WriteLine("Esta seguro que desea eliminar todos los elementos mostrados en la lista: s/n");
+            string responce = Console.ReadLine();
+            if (responce == "s" || responce == "S")
+            {
+                vehiculos = new List<Vehiculo>();
+                this.totalv = 0;
+                Console.WriteLine("Se han eliminado los objetos correctamente");
+            }
+            else
+            {
+                Console.WriteLine("No se elimino. hay " + totalv + "vehiculos rsgitrados");
+            }
+            Console.ReadLine();
+
+        }
+
+        public void eliminarvehiculo(Vehiculo V)
+        {
+            vehiculos.Remove(V);
+
+            totalv -= 1;
+            Console.WriteLine("Se ha eliminado un registro.");
+        }
+
+
+
+
+
+    }
 
 
 
@@ -87,27 +143,39 @@ namespace POO.ejer2
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("De la capacidad de disco de su computadora");
-            int cp = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("De el espacio disponible de su computadora");
-            int ed = int.Parse(Console.ReadLine());
-            Console.WriteLine("De el dato en GB a añadir");
-            int dato1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("De el dato en GB a eliminar");
-            int dato2 = int.Parse(Console.ReadLine());
+            CasaComercial cc2 = new CasaComercial("autolote el chele", "contacto@elchele.com", 0, null);
+            cc2.añadirVehiculo(new Vehiculo(2, "hilux", "toyota", 4000, 6000, 2015));
+            cc2.eliminarvehiculo(new Vehiculo());
+            cc2.Mostrarvehiculo();
+            cc2.Vaciarvehiculos();
 
 
+            //Definimos un nuevo objeto de tipo casa comercial
+            Console.WriteLine("De el nombre de la casa comercial");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el email");
+            string email = Console.ReadLine();
 
-            Computadora comp = new Computadora(cp, ed, false);
-            comp.Encendida();
-            comp.Añadirdatos(dato1);
-            comp.Eliminardatos(dato2);
-            comp.Apagar();
+            CasaComercial cc = new CasaComercial(nombre, email, 0, new List<Vehiculo>());
 
-            Console.ReadKey();
+            //informacion del nuevo vehiculo a añadir
+            Console.WriteLine("De la informacion del primer vehiculo a añadir");
+            Console.WriteLine("Modelo: ");
+            string modelo = Console.ReadLine();
+            Console.WriteLine("Marca: ");
+            string marca = Console.ReadLine();
+            Console.WriteLine(" Kilometraje: ");
+            int km = int.Parse(Console.ReadLine());
+            Console.WriteLine("Año: ");
+            int año = int.Parse(Console.ReadLine());
+            Console.WriteLine("Precio: ");
+            decimal precio = decimal.Parse(Console.ReadLine());
+
+            //procedemos a añadir la informacion del nuevo vehiculo haciendo uso del metodo Añadirvehiculo
+            //definido en la clase casa comercial
+
+            cc.añadirVehiculo(new Vehiculo(1, marca, modelo, km, precio, año));
+
 
         }
     }
---
+++ POO.ejer3/POO.ejer3/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.ejer3
+{
+    class Computadora
+    {
+
+        int capadisco;
+        int espadisponible;
+        bool encendido;
+
+
+
+        public Computadora(int capadisco, int espadisponible, bool encendido)
+        {
+            this.Capadisco = capadisco;
+            this.Espadisponible = espadisponible;
+            this.Encendido = encendido;
+        }
+
+        public int Capadisco { get => capadisco; set => capadisco = value; }
+        public int Espadisponible { get => espadisponible; set => espadisponible = value; }
+        public bool Encendido { get => encendido; set => encendido = value; }
+        public void Añadirdatos(int datos)
+        {
+            if (encendido)
+            {
+                if (datos <= espadisponible)
+                {
+                    espadisponible -= datos;
+                    Console.WriteLine("Se añadio " + datos + " GB de datos al disco duro.\nEspacio disponible " + espadisponible + " GB");
+                }
+                else
+                {
+                    espadisponible = 0;
+                    Console.WriteLine("No hay espacio disponible");
+                }
+            }
+            else
+            {
+                Console.WriteLine("La computadora esta apagada");
+            }
+        }
+
+        public void Eliminardatos(int datos)
+        {
+            if (encendido)
+            {
+                if (datos <= (capadisco - espadisponible))
+                {
+                    espadisponible += datos;
+
+                    Console.WriteLine("Se elimino " + datos + " GB de datos del disco duro");
+                }
+                else
+                {
+                    espadisponible = capadisco;
+                    Console.WriteLine("No hay datos suficiente almacenados en el dico duro para eliminar. \n Espacio disponible " + espadisponible);
+                }
+            }
+            else
+            {
+                Console.WriteLine("La computadora esta apagada");
+            }
+        }
+
+        public void Apagar()
+        {
+            encendido = false;
+            Console.WriteLine("La computadora esta apagada");
+        }
+        public void Encendida()
+        {
+            encendido = true;
+            Console.WriteLine("La computadora esta encendida");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("De la capacidad de disco de su computadora");
+            int cp = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("De el espacio disponible de su computadora");
+            int ed = int.Parse(Console.ReadLine());
+            Console.WriteLine("De el dato en GB a añadir");
+            int dato1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("De el dato en GB a eliminar");
+            int dato2 = int.Parse(Console.ReadLine());
+
+
+
+            Computadora comp = new Computadora(cp, ed, false);
+            comp.Encendida();
+            comp.Añadirdatos(dato1);
+            comp.Eliminardatos(dato2);
+            comp.Apagar();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
